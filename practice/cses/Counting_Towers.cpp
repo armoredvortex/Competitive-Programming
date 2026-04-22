144eb30a23e228aeb9da846148584e14c7528c3f
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2026-04-22 22:11:55 +0530

    april 22

--- practice/cses/Counting_Towers.cpp
+++ practice/cses/Counting_Towers.cpp
@@ -13,31 +13,35 @@ typedef vector<long long> vll;
 #endif
 
 const int MOD = 1000000007;
-void solve()
-{
-    ll n;
-    cin >> n;
-    ll c = 2;
-    for (ll i = 2; i <= n; i++)
-    {
-        c = (4 * c) % MOD;
-    }
-    cout << c << '\n';
-}
 
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
+    vector<int> ans(1000010);
+    vector<vector<ll>> dp(1000010, vector<ll>(2));
+    dp[0] = {1, 1};
+    for (int i = 1; i < 1000010; i++)
+    {
+        dp[i][0] = ((dp[i - 1][0] * 4ll) % MOD + (dp[i - 1][1]) % MOD) % MOD;
+        dp[i][1] = ((dp[i - 1][1] * 2ll) % MOD + (dp[i - 1][0] % MOD)) % MOD;
+        ans[i] = (dp[i][0] + dp[i][1]) % MOD;
+    }
+
     int t;
     cin >> t;
     while (t--)
-        solve();
+    {
+        int n;
+        cin >> n;
+        if (n == 1)
+        {
+            cout << 2 << '\n';
+        }
+        else
+        {
+            cout << ans[n - 1] << '\n';
+        }
+    }
 }
-
-// n-1 cuts allowed,
-// n-1C0 + n-1C1 + n-1C2 + ... = 2^n-1
-
-// 2x1 horizontal piece
-// n-2 * 1 + n-3*2 + n-4*3 + ...
